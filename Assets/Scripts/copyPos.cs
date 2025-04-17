716aa58b33c7d6607ed5353210370cd8fd739975
nick0900 <90617148+nick0900@users.noreply.github.com>
2025-04-17 15:33:48 +0200

    feat: 6 scenes made in total

--- Assets/Scripts/copyPos.cs
+++ Assets/Scripts/copyPos.cs
@@ -7,9 +7,23 @@ public class copyPos : MonoBehaviour
 {
     public Transform otherT;
 
+    public bool copyX = true;
+    public bool copyY = true;
+    public bool copyZ = true;
+
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = otherT.position;
+        if (otherT != null)
+        {
+            Vector3 temp = this.transform.position;
+            if (copyX)
+                temp.x = otherT.position.x;
+            if (copyY)
+                temp.y = otherT.position.y;
+            if (copyZ)
+                temp.z = otherT.position.z;
+            this.transform.position = temp;
+        }
     }
 }
